16d57cab950a55f6da4d2f22c2b6712715b8a498
Gia Jennings <gjennings.co@gmail.com>
2020-12-09 13:02:20 -0500

    build file structure

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require "net/http"
+require "open-uri"
+require "pry"
+
+require_relative "../lib/modules/nba"
+require_relative "../lib/cli"
+require_relative "../lib/api"
+require_relative "../lib/teams"
